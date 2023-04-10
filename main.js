7e9888ed6ae8296fdeb0fbefaea2b5b0f87bf107
helen-zhou-8 <helenzhou0111@gmail.com>
2023-04-10 14:37:19 -0400

    basic setup, gets display input

---
+++ main.js
@@ -0,0 +1,62 @@
+// we're display 2 but that one was down when i coded this so i just used display 1 for now -helen
+var host = "cpsc484-01.yale.internal:8888";
+
+$(document).ready(function () {
+    frames.start();
+    twod.start();
+});
+
+// get the frames from the display
+var frames = {
+    socket: null,
+
+    start: function () {
+        var url = "ws://" + host + "/frames";
+        frames.socket = new WebSocket(url);
+        frames.socket.onmessage = function (event) {
+            frames.show(JSON.parse(event.data));
+        }
+    },
+
+    show: function (frame) {
+        console.log(frame);
+    }
+};
+
+// get the raw image data from the display
+var twod = {
+  socket: null,
+
+  // create a connection to the camera feed
+  start: function () {
+      var url = "ws://" + host + "/twod";
+      twod.socket = new WebSocket(url);
+
+      // whenever a new frame is received...
+      twod.socket.onmessage = function (event) {
+
+          // parse and show the raw data
+          twod.show(JSON.parse(event.data));
+      }
+  },
+
+  // show the image by adjusting the source attribute of the HTML img object previously created
+  show: function (twod) {
+      $('img.twod').attr("src", 'data:image/pnjpegg;base64,' + twod.src);
+  },
+};
+
+// set up the basic canvas
+function setup() {
+  // get the dimensions of the parent HTML element
+  height = document.getElementById('sketch-holder').clientHeight;
+  width = document.getElementById('sketch-holder').clientWidth;
+
+  // create canvas
+  var canvas = createCanvas(width, height);
+
+  // stretch canvas to fit dimensions of parent
+  canvas.parent('sketch-holder');
+  canvas.width = width;
+  canvas.height = height;
+}
